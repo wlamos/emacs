d2b14e9e4aa3ea0dbe9301605b7dc972c205aa95
Wenliang Zhang <wlamos@gmail.com>
2011-05-17 20:58:56 +0800

    some change for macos

--- config-init.el
+++ config-init.el
@@ -1,11 +1,21 @@
 (setq inhibit-default-init t)
-
 (setq load-path (append
 		 '("~/work/github/emacs"
 		   "~/work/github/emacs/site-lisp"
 		   "~/work/github/emacs/site-lisp/color-theme"
+		   "~/work/github/emacs/site-lisp/nxml-mode-20041004"
+		   "~/work/github/emacs/site-lisp/company"
+		   "~/share/emacs/site-lisp"
 		   )
 		 load-path))
+
+(defmacro require-maybe (feature &optional file)
+  "*Try to require FEATURE, but don't signal an error if `require' fails."
+  `(require ,feature ,file 'noerror))
+
+(defmacro when-available (func foo)
+  "*Do something if FUNCTION is available."
+  `(when (fboundp ,func) ,foo))
 
 (defun load-file-list (file-list)
   "*Load file list FILE-LIST in order."
